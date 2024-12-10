9ec113a240ad514b6b1117fb7503b7eb55be5471
Miguel Albert <miguelalbert38@gmail.com>
2024-12-10 02:12:12 -0300

    styled components done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import logo from "../../assets/Ignite-simbol.svg"
+import { NewTransactionModal } from "../NewTransactionModal"
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
+import * as Dialog from "@radix-ui/react-dialog"
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
